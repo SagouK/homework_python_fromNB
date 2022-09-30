93bd116f69b1360de2d8d0c421bda5da46930f13
Anastasia <anastasiyazadybinina@gmail.com>
2022-09-30 14:35:37 +0500

    task_2

--- hw_2/task_2.py
+++ hw_2/task_2.py
@@ -1,4 +1,17 @@
-a = 0.9
-if a % 10 > 9:
-    a = a * 10 
-    print(a)
+# Задача 2. Напишите программу, которая принимает на вход число N
+# и выдает набор произведений чисел от 1 до N.
+
+def calculate_numbers(user_number):
+    counter = 1
+    arr = []
+    arr.append(counter)
+    for i in range(2,user_number + 1):
+        counter = counter * i
+        arr.append(counter)
+    return arr
+
+try:
+    num = int(input("Введите целое число: "))
+    print(calculate_numbers(num))
+except:
+    print('Ошибка ввода данных!')
